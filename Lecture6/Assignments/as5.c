bfb3fe1c7c9051d6e861b1a026b9e358b39326b1
nico <90756549+sengokus@users.noreply.github.com>
2022-04-29 09:26:20 +0800

    Add files via upload

---
+++ Lecture6/Assignments/as5.c
@@ -0,0 +1,12 @@
+/* Finds the length of an array. */
+
+#include<stdio.h>
+
+int main(void) {
+    int number[16];
+
+    size_t n = sizeof(number)/sizeof(number[0]);
+    printf("Total elements the array can hold is: %d\n", n);
+
+    return 0;
+}
